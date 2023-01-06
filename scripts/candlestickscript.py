0e87141996067a4b6c44f5584d77d355d080e99d
joshelb <joshi31@gmx.de>
2023-01-06 22:15:52 +0000

    reh

--- scripts/candlestickscript.py
+++ scripts/candlestickscript.py
@@ -1,40 +1,23 @@
 from sqlalchemy import create_engine
 import pandas as pd
+from datetime import datetime
 
 
 db_connection_str = 'mysql+pymysql://joshelb:chirurgie@localhost/userInfo'
 db_connection = create_engine(db_connection_str)
 
-df = pd.read_sql('SELECT * FROM tradeHistoryKISMJOSH', con=db_connection)
-print(df)
+tick_data = pd.read_sql('SELECT * FROM tradeHistoryKISMJOSH', con=db_connection)
+tick_data["timestamp"] = tick_data["timestamp"].apply(lambda x: datetime.fromtimestamp(int(x)))
+tick_data.set_index('timestamp', inplace=True, drop=True)
 
+convert_dict = {'price': float,
+                'quantity': float
+                }
 
+tick_data = tick_data.astype(convert_dict)
+print(tick_data.dtypes)
+bar = tick_data["price"].resample("1Min").ohlc()
+vol = tick_data["quantity"].resample("1Min").sum()
+bar["quantity"] = vol
 
-
-
-
-
-
-
-"""
-timeframe = '1min'
-
-tick_data['DATETIME'] = pd.to_datetime(tick_data['DATE'] + ' ' + tick_data['TIME'])
-tick_data.set_index('DATETIME', inplace=True)
-
-ohlcv_data = pd.DataFrame(columns=[
-    'SYMBOL_N',
-    'open',
-    'high',
-    'low',
-    'close',
-    'volume'])
-
-for symbol in tick_data['SYMBOL_N'].unique():
-    ohlcv_symbol =  tick_data.loc[tick_data['SYMBOL_N'] == symbol, 'PRICE'].resample(timeframe).ohlc()
-    ohlcv_symbol['SYMBOL_N'] = symbol
-    ohlcv_symbol['volume'] = (tick_data.loc[tick_data['SYMBOL_N'] == symbol, 'VOLUME'].resample(timeframe).max() - tick_data.loc[tick_data['SYMBOL_N'] == symbol, 'VOLUME'].resample(timeframe).max().shift(1))
-    ohlcv_data = ohlcv_data.append(ohlcv_symbol, sort=False)
-
-print(ohlcv_data)
-"""
+print(bar)
